8682ed8d9ac026473d5d0d218ad2b3f063164781
batherton <brian@asimail.com>
2015-08-03 18:50:06 -0400

    1

--- js/index.js
+++ js/index.js
@@ -259,3 +259,20 @@ function CloseCustomer(){
  document.getElementById('Customer').className = "HideSection";
 }
 
+
+function Camera(){
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+	    destinationType: Camera.DestinationType.DATA_URL
+	});
+}
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+}
