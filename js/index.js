ff4922acac52465df0b1fce390ee2711f2b3211b
batherton <brian@asimail.com>
2015-08-03 15:02:58 -0400

    1

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
 
-        receivedElement.setAttribute('style', 'display:block;');
+        //receivedElement.setAttribute('style', 'display:block;');
 
 
 
