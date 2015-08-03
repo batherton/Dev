4bc1a4076215ab3cb8797d55f76a592848fafc90
batherton <brian@asimail.com>
2015-08-03 01:47:43 -0400

    1

--- js/index.js
+++ js/index.js
@@ -100,8 +100,8 @@ function onCurLocSuccess(position) {
 };
 
 function onCurLocError(error) {
-    //alert('code: '    + error.code    + '\n' +
-    //      'message: ' + error.message + '\n');
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
 };
 
 
