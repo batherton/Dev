26d2d928889f9f3f300833b0f8e787c1ffd399a2
batherton <brian@asimail.com>
2015-08-10 01:33:26 -0400

    1

--- js/index.js
+++ js/index.js
@@ -306,19 +306,6 @@ function callbackfunc(){
 
 
 
-function barcodescan(){
-   cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
 
-}
 
 
