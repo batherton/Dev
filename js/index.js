a23945e9dd70307688962cbaf205343de560fde5
batherton <brian@asimail.com>
2015-08-10 22:26:59 -0400

    2

--- js/index.js
+++ js/index.js
@@ -18,25 +18,7 @@ var app = {
     onDeviceReady: function() {
 
         app.receivedEvent('deviceready');
-
-
-
-   cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
-
-
-
-
-
+	screen.lockOrientation('landscape');
 	cordova.plugins.backgroundMode.enable();
 	cordova.plugins.backgroundMode.onactivate = function () {
 		//alert('test background');
