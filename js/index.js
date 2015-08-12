a5ce343a9700c02ec0add6e3df86dedaaa282184
batherton <brian@asimail.com>
2015-08-12 17:21:13 -0400

    1

--- js/index.js
+++ js/index.js
@@ -20,6 +20,38 @@ var app = {
         app.receivedEvent('deviceready');
 	screen.lockOrientation('portrait');
 
+
+
+
+	window.plugins.flashlight.available(function(isAvailable) {
+	  if (isAvailable) {
+	    alert('Is Available');
+	    // switch on
+	    window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+
+	    // switch off after 3 seconds
+	    setTimeout(function() {
+	      window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+	    }, 3000);
+
+	  } else {
+	    alert("Flashlight not available on this device");
+	  }
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //--------------------------------------------------------------
 
             window.navigator.geolocation.getCurrentPosition(function(location) {
